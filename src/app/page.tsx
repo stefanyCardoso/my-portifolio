008ef0219a3d0464bb1a99c47a8bf0835d0c82a6
stefany cardoso <stefanycoliveira20@gmail.com>
2023-09-03 18:41:37 -0300

    feat: new section and adding new font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Header from './componentes/Header/inde'
+import './styles.scss'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <section className='info'>
+        <div className='text'>
+          <h1>Stéfany Cardoso</h1>
+          <h2>Desenvolvedora front-end</h2>
+          <ul>
+            <li>Javascript</li>
+            <li>CSS</li>
+            <li>HTML</li>
+          </ul>
+          <button>Contato</button>
+        </div>
+        <div className='image'>
+          imagem
+        </div>
+      </section>
     </main>
   )
 }
